a49ec8444bdb8ebc903a18bffca78699d03ce0b9
Vijay P <60179441+vijaysandy007@users.noreply.github.com>
2022-06-05 00:27:46 +0530

    15 min slot seprated

--- controller/SlotSeprate/seprate_slot.js
+++ controller/SlotSeprate/seprate_slot.js
@@ -1,43 +1,60 @@
-const getSeprateSchema = require('../../mongoose_schema/seprate_allocate')
 const getEmployeeAllocate = require('../../mongoose_schema/employee_profile_alocate_slot')
+const moment = require('moment')
 
 const getAllocateList = async (req, res) => {
-    const findSlots = await getEmployeeAllocate.find({ employee_id: req.params.id })
+  const findSlots = await getEmployeeAllocate.find({ employee_id: req.params.id })
 
-    const dateObj = new Date(req.body.date)
+  const dateObj = new Date(req.body.date)
+  const Myyear = dateObj.getUTCFullYear()
+  const Mymonth = dateObj.getUTCMonth()
+  const Mydate = dateObj.getUTCDate()
 
-    const Myyear = dateObj.getUTCFullYear()
-    const Mymonth = dateObj.getUTCMonth()
-    const Mydate = dateObj.getUTCDate()
+  const myDate = `${Myyear}-${formatDate(Mymonth)}-${formatDate(Mydate)}`
+  var ResponseDate;
+  var FiteenResult = []
+  var thirtyResult=[]
+  findSlots.forEach(data => {
+    const resDateObj = new Date(data.date)
+    const Responseyear = resDateObj.getUTCFullYear()
+    const Responsemonth = resDateObj.getUTCMonth()
+    const Responsedate = resDateObj.getUTCDate()
 
-    const myDate = `${Myyear}-${formatDate(Mymonth)}-${formatDate(Mydate)}`
-    var ResponseDate
-    findSlots.forEach(date => {
-        console.log(date.date)
+    ResponseDate = `${Responseyear}-${formatDate(Responsemonth)}-${formatDate(Responsedate)}`
 
-        const resDateObj = new Date(date.date)
-        const Responseyear = resDateObj.getUTCFullYear()
-        const Responsemonth = resDateObj.getUTCMonth()
-        const Responsedate = resDateObj.getUTCDate()
+    data.alloacte.forEach(allocate => {
+      const from = new Date(allocate.from)
+      const to = new Date(allocate.to)
+      const start = moment(from, 'YYYY-MM-DD hh:mm a')
+      const end = moment(to, 'YYYY-MM-DD hh:mm a')
+      start.minute(Math.ceil(start.minutes() / 15) * 15)
 
-        ResponseDate = `${Responseyear}-${formatDate(Responsemonth)}-${formatDate(Responsedate)}`
-        //   ResponseDate == myDate ? console.log('Correct') : console.log('Wrong');
+      // const thirtyStart =  moment(from, 'YYYY-MM-DD hh:mm a')
+      // const thirtyEnd = moment(to, 'YYYY-MM-DD hh:mm a')
+
+      start.minute(Math.ceil(start.minutes() / 15) * 15)
+
+      while (start <= end) {
+        FiteenResult.push(moment.utc(start))
+        start.add(15, 'minutes');
+
+      }
+      return FiteenResult
+      
     })
+  })
 
-    // const myDate = findSlots.forEach(findDate =>{
-    //    console.log(findDate.date)
-    // })
+  if (ResponseDate == myDate) {
 
+    res.status(200).json({ data: FiteenResult })
 
-    res.status(200).json({ data: findSlots })
+  }
 }
-
 
 
 module.exports = {
-    getAllocateList
+  getAllocateList
 }
 
 function formatDate(data) {
-    return data < 10 ? `0${data}` : data
+  return data < 10 ? `0${data}` : data
 }
