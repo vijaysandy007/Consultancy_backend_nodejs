56340b69b4215556e69648a39757bb6aa8b77474
Vijay P <60179441+vijaysandy007@users.noreply.github.com>
2022-06-05 11:14:51 +0530

    error response added

--- controller/User-Appointment/appointment_controller.js
+++ controller/User-Appointment/appointment_controller.js
@@ -2,7 +2,7 @@ const appointmentSchema = require('../../mongoose_schema/user_appointment')
 const service = require('../../mongoose_schema/service_schema');
 
 const findData = async (req, res) =>{
-    const getData = await appointmentSchema.find().populate('service_id').then(data =>{
+    const getData = await appointmentSchema.findOne({}).populate('service_id').then(data =>{
         res.status(200).json({data:data})
     })
 }
